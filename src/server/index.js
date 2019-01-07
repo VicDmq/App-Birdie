e639d409f5e5a3651294c0dae7454d878cfe1798
Victor Domecq <vdomecq@ensc.fr>
2019-01-07 12:24:21 -0500

    Test heroku

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ import { appConfig } from "../../server.config";
 const PORT = process.env.PORT || appConfig.server.port;
 var server_host = process.env.HOST || "0.0.0.0";
 
-server.listen(PORT, function() {
+server.listen(PORT, server_host, function() {
   console.log(
     "Server launched successfully : listening on " + PORT + " " + server_host
   );
